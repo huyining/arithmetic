16e85bfc04b7cdf88d5d0a07ad1c97ab5ce828a8
胡宜宁 <dolphin@xingren.com>
2019-08-06 22:47:28 +0800

    二分查找树广度优先遍历和深度优遍历实现&&使用递归查询二分树最小节点和最大节点

--- src/www/arithmetic/imooc/com/binsertree/BinarySearchTree.java
+++ src/www/arithmetic/imooc/com/binsertree/BinarySearchTree.java
@@ -1,5 +1,11 @@
 package www.arithmetic.imooc.com.binsertree;
 
+import com.sun.tools.hat.internal.model.Root;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 二分查找树
  *
@@ -111,4 +117,187 @@ public class BinarySearchTree<E extends Comparable<E>> {
             return contains(node.right, e);
         }
     }
+
+    /**
+     * 二分查找树的前序遍历
+     */
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    /**
+     * 递归实现树的遍历, 深度优先   遍历
+     *
+     * @param node
+     */
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+
+    /**
+     * 中序遍历
+     */
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    /**
+     * 中序遍历
+     *
+     * @param node
+     */
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    /**
+     * 后续遍历
+     */
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    /**
+     * 后续遍历
+     *
+     * @param node
+     */
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+
+
+    /**
+     * 链表的广度优先
+     */
+    public void levelOrder() {
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node node = queue.remove();
+            System.out.println(node.e);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    /**
+     * 获取最小节点数数据
+     *
+     * @return
+     */
+    public E minimum() {
+        if (root == null) {
+            throw new IllegalArgumentException("BinarySearchTree is empty.....");
+        }
+
+        Node node = minimum(root);
+        return node.e;
+    }
+
+    /**
+     * 获取最小节点的Node
+     *
+     * @param node
+     * @return
+     */
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+
+    /**
+     * 获取最大节点的元素
+     *
+     * @return
+     */
+    public E maxmum() {
+        if (root == null) {
+            throw new IllegalArgumentException("BinarySearchTree  is empty....");
+        }
+        return maxmum(root).e;
+    }
+
+    /**
+     * 获取二分查找树中祖代的元素
+     *
+     * @param node
+     * @return
+     */
+    private Node maxmum(Node node) {
+        if (node.right == null) {
+            return node;
+        }
+
+        return maxmum(node.right);
+    }
+
+
+    /**
+     * 打印二分树
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+
+    /**
+     * 生成以node为根节点，深度为depth的描述二叉树的字符串
+     */
+    private void generateBSTString(Node node, int depth, StringBuilder res) {
+
+        if (node == null) {
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth) + node.e + "\n");
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+    }
+
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            res.append("--");
+        }
+        return res.toString();
+    }
+
+
 }
